a88815ec108d451849547f87e360adbe23db9481
bernardoduartes <bernardoduartes@gmail.com>
2024-03-06 11:51:33 -0300

    feat: busca de tados os produtos

--- src/main/java/br/com/bds/model/Product.java
+++ src/main/java/br/com/bds/model/Product.java
@@ -1,2 +1,19 @@
-package br.com.bds.model;public class Product {
+package br.com.bds.model;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+public class Product extends PanacheEntity {
+    public String name;
+    public BigDecimal price;
+    @CreationTimestamp
+    public Date createAt;
+    @UpdateTimestamp
+    public Date updatedAt;
 }
--- src/main/java/br/com/bds/resource/ProductResource.java
+++ src/main/java/br/com/bds/resource/ProductResource.java
@@ -1,2 +1,21 @@
-package br.com.bds.resource;public class ProductResource {
+package br.com.bds.resource;
+
+import br.com.bds.model.Product;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.List;
+
+@Path("products")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class ProductResource {
+
+    @GET
+    public List<Product> findAll(){
+        return Product.listAll();
+    }
 }
